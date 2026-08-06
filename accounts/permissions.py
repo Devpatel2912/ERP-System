7543a97eb802ee65d25dd88de013f7d0b7ebd3e9
Devpatel2912 <dev29123@gmail.com>
2026-08-06 16:46:52 +0530

    feat: implement employee management page and remove server-side manage.py script

--- accounts/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsAdmin(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.role == 'admin'
-
-class IsEmployee(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.role == 'employee'
